31ef4d289366f377e17a0274e4850fdad01132f9
test <lwcworld123@naver.com>
2017-08-07 23:11:27 +0900

    new

--- main_mesh.m
+++ main_mesh.m
@@ -1,30 +1,29 @@
 clc;clear;close all
-% param
+% param setting
 param.dim = 2;
 param.m = 2;
 param.n = 2;
 param.xi = 1;
 param.umeta = 1;
 param.rho0 = 0.5;
-
 param.q_goal = [0;0];
-param.q_obs = [1;0];
+param.q_obs = [1,0;0,1;-2,0]';
 
 % set sinario
-d = -3:0.1:3;
-[xs, ys] = meshgrid(d);
+space = -4:0.1:4;
+[xs, ys] = meshgrid(space);
 
 %%
 [U_mesh, ax_mesh, ay_mesh] = potential_path(xs, ys, param);
-[ax, ay] = gradient(U_mesh, 0.001, 0.001);
+% [ax, ay] = gradient(U_mesh, 0.001, 0.001);
 
-%%
+%% plot
 figure(1)
 mesh(xs,ys,U_mesh)
 grid on
 
 figure(2)
-contour(d, d, U_mesh)
+contour(space, space, U_mesh)
 hold on
 scale = 0.1;
 ax_mesh = ax_mesh./ sqrt((ax_mesh.^2 + ay_mesh.^2));
@@ -32,9 +31,3 @@ ay_mesh = ay_mesh./ sqrt((ax_mesh.^2 + ay_mesh.^2));
 quiver(xs, ys, ax_mesh*scale, ay_mesh*scale, 'AutoScale', 'on')
 grid on
 legend('a', 'b')
-
-% figure(3)
-% mesh(xs, ys, ax_mesh)
-% 
-% figure(4)
-% mesh(xs, ys, ay_mesh)
